80fb4167031e060cb5eca2b3d80efcc9987e1f92
ArianeAlves <arianecamilo.alves@gmail.com>
2017-10-24 16:41:09 -0200

    projeto completo

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ var comecou = false;
 var letrasChutadas = [];
 var palavra="";
 
-$("#btnCadastrar").click(function (event) {
+function aoCadastrar(event) {
     event.preventDefault();
     // alert("mensagem marota");  
 
@@ -33,10 +33,12 @@ $("#btnCadastrar").click(function (event) {
     }
 
     comecou = true;
-});
+}
+
+$("#btnCadastrar").click(aoCadastrar);
 
 
-
+var erro=0;
 $(document).keydown(function (event) {
 
     if (comecou) {
@@ -65,14 +67,31 @@ $(document).keydown(function (event) {
             //Se a letra que pressionei for igual a letra da palavra
             if(letra == letra2){
                 //i é o índice que temos que mostrar na tela
-                $(".letras span").eq(i).addClass("visivel");
+                $(".letras span").eq(i).addClass("visivel");//.eq significa pegar um únco elemento e devo apontar o índice
             }
         }
         //Se a palavra estiver completa
+        if($(".letras span:not(.visivel)").length == 0){
+            //Mostra o final correto
+            $("#ganhou").addClass("visivel");
+            $("#forca").removeClass("visivel");
+        }
+        
+
         //Mostra o final correto
         }else{
          //Se não 
         //mostra o mebro do boneco
+        if (erro<6){
+            $(".corpo .st0").eq(erro).attr("class", "st0 visivel");
+            erro++;
+        }
+        else{
+            $("#forca").removeClass("visivel");
+            $("#perdeu").addClass("visivel");
+         
+        }
+
         //Se excede tentativas
         //Mostra a família triste
 
@@ -81,3 +100,17 @@ $(document).keydown(function (event) {
     }
 });
 
+
+$(".btn-recomecar").click(function(event){
+   $("#ganhou").removeClass("visivel");
+   $("#perdeu").removeClass("visivel");
+   $("#cadastro").addClass("visivel");
+
+   $("#palavra").val("");
+   $(".letras").html("");
+   $(".letras-usadas").html("");
+   $(".corpo .st0").attr("class","st0");
+   letrasChutadas = [];
+   erro = 0;
+   comecou = false;
+});
